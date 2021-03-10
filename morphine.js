0dfc560121e91612c231ef75d3f081ee4dfe7fd8
avinash-idiosnow <74093466+avinash-idiosnow@users.noreply.github.com>
2021-03-10 19:05:58 +0530

    Create morphine.js

---
+++ morphine.js
@@ -0,0 +1,54 @@
+;(function initMorphine() {
+  'use strict';
+
+  function getExpectedArguments(fn) {
+    var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+    var text = fn.toString();
+    var args = text.match(FN_ARGS)[1].split(',');
+    args = args.map(function (name) {
+      return name.trim();
+    });
+    return args;
+  }
+
+  function morphine(obj, methodName, dependencies) {
+    var original;
+
+    if (typeof obj === 'function') {
+      original = obj;
+      dependencies = methodName;
+    } else {
+      original = obj[methodName];
+    }
+
+    dependencies = dependencies || {};
+
+    var expected = getExpectedArguments(original);
+
+    var proxy = function () {
+      var runtimeArgs = Array.prototype.slice.call(arguments, 0);
+      var parameters = expected.map(function (name) {
+        if (dependencies.hasOwnProperty(name)) {
+          return dependencies[name];
+        } else {
+          return runtimeArgs.shift();
+        }
+      });
+      // console.log('parameters', parameters);
+      return original.apply(obj, parameters);
+    };
+
+    if (typeof obj !== 'function') {
+      obj[methodName] = proxy;
+    }
+    return proxy;
+  }
+
+  if (typeof module === 'object') {
+    module.exports = morphine;
+  }
+  if (typeof window === 'object') {
+    /* global window: true */
+    window.morphine = morphine;
+  }
+}());
